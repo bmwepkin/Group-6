8e8fa161d8ee43fe1cabeb6a0df06b9844171e01
jpattrsn <jrp9686@gmail.com>
2019-09-30 21:09:01 -0500

    Add files via upload

--- GP_Script.R
+++ GP_Script.R
@@ -1,5 +1,8 @@
 #set working directory to folder where csv file is downloaded
 #install readr package
 library(readr)
-df <- read_csv("Police_Incidents.csv", col_types = cols(`Hate Crime` = col_character(), `Victim Business Phone` = col_character()))
+#install dplyr
+library(dplyr)
 
+df <- read_csv("Police_Incidents.csv", col_types = cols(`Hate Crime` = col_character(), `Victim Business Phone` = col_character()))
+df <- filter(df, `Year of Incident` == 2018)
