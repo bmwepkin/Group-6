6090ed421aa60dba6b851587fdab1a49dc5787e8
kumkalk2k19 <viswanathan-subramanian@uiowa.edu>
2019-10-13 09:24:15 -0500

    Update GP_Script.R
    After loading the main csv file, filter it for the year 2018 and write it to another csv to load it faster next time.

--- GP_Script.R
+++ GP_Script.R
@@ -1,8 +1,12 @@
 #set working directory to folder where csv file is downloaded
-#install readr package
-library(readr)
-#install dplyr
-library(dplyr)
 
-df <- read_csv("Police_Incidents.csv", col_types = cols(`Hate Crime` = col_character(), `Victim Business Phone` = col_character()))
-df <- filter(df, `Year of Incident` == 2018)
+# Load Libraries
+library(readr)
+suppressPackageStartupMessages(library(dplyr))
+library(reshape2)
+suppressPackageStartupMessages(library(ggplot2))
+
+# df <- read_csv("Police_Incidents.csv", col_types = cols(`Hate Crime` = col_character(), `Victim Business Phone` = col_character()))
+# df <- filter(df, `Year of Incident` == 2018)
+# write_csv(df, "Police_Incidents_2018.csv")
+
